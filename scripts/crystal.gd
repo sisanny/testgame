c1831a123dac03d7d9f76a90cf7fe352aafa334d
Lilla Csapo <lantasire@gmail.com>
2026-01-30 10:34:29 +0000

    Adding crystal score

--- scripts/crystal.gd
+++ scripts/crystal.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var hud = %HUD
 
 func _on_body_entered(_body: Node2D) -> void:
 	animation_player.play("pickup")
+	hud.add_point()
